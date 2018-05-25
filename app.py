8426a3e979a45596fd7347dca12dbfbb6ad57fad
RobertYE <wu87988622@gmal.com>
2018-05-25 17:46:00 +0800

    lint bot test

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def get_google_image(text):
         'upgrade-insecure-requests': "1",
         'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
         'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        'accept-encoding': "gzip, deflate, br",
         'accept-language': "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5",
         'cache-control': "no-cache",
     }
