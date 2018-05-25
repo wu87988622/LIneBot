e2b0ece60770e6b60be1281f3a1abe267985ced2
RobertYE <wu87988622@gmal.com>
2018-05-25 14:19:41 +0800

    lint bot test

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = event.message.text
     if message is '貼圖':
         sendMsg = StickerSendMessage(package_id='1033962', sticker_id='1435740')
         line_bot_api.reply_message(event.reply_token, sendMsg)
