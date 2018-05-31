f6dd7a9b3b8ae191dc14421a9ffaa1c2da4aa59f
RobertYE <wu87988622@gmal.com>
2018-05-31 09:23:59 +0800

    update

--- app.py
+++ app.py
@@ -184,6 +184,9 @@ def handle_message(event):
     elif message == 'ㄤ':
         sendMsg = TextSendMessage(text='ㄤㄤ泥豪')
         line_bot_api.reply_message(event.reply_token, sendMsg)
+    elif message == '叫大哥':
+        sendMsg = TextSendMessage(text='大哥好')
+        line_bot_api.reply_message(event.reply_token, sendMsg)
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
