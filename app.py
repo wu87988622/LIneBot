a453951b05f5b37865a3b03e87e00d54ac029ec6
RobertYE <wu87988622@gmal.com>
2018-06-05 17:35:33 +0800

    update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from switch import switch
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 
-from linebot.models import TextMessage, MessageEvent, TextSendMessage, StickerSendMessage, ImageSendMessage, VideoSendMessage, TemplateSendMessage, PostbackTemplateAction, ImageCarouselTemplate, ImageCarouselColumn
+from linebot.models import TextMessage, MessageEvent, TextSendMessage, StickerSendMessage, ImageSendMessage, VideoSendMessage, TemplateSendMessage, MessageTemplateAction, ImageCarouselTemplate, ImageCarouselColumn
 
 import requests
 
@@ -244,26 +244,30 @@ def handle_message(event):
                         columns=[
                             ImageCarouselColumn(
                                 image_url='http://i.imgur.com/tmlKy.jpg',
-                                action=PostbackTemplateAction(
-                                    label='志鈞哥'
+                                action=MessageTemplateAction(
+                                    label='志鈞哥',
+                                    text='志鈞哥'
                                 )
                             ),
                             ImageCarouselColumn(
                                 image_url='http://pic.pimg.tw/peace543/1371744369-3823290642.jpg',
-                                action=PostbackTemplateAction(
-                                    label='小弟錯了'
+                                action=MessageTemplateAction(
+                                    label='小妹錯了',
+                                    text='小妹錯了'
                                 )
                             ),
                             ImageCarouselColumn(
                                 image_url='http://img.ltn.com.tw/Upload/ent/page/800/2014/06/03/phpgJHXH4.jpg',
-                                action=PostbackTemplateAction(
-                                    label='只好露出'
+                                action=MessageTemplateAction(
+                                    label='只好露出',
+                                    text='只好露出'
                                 )
                             ),
                             ImageCarouselColumn(
                                 image_url='http://i.imgur.com/Iw3jEQ4.jpg',
-                                action=PostbackTemplateAction(
-                                    label='胸部'
+                                action=MessageTemplateAction(
+                                    label='胸部',
+                                    text='胸部'
                                 )
                             )
                         ]
