a6977df7541a8e056eed57b024fcc23b3a824fb1
RobertYE <wu87988622@gmal.com>
2018-06-15 14:42:42 +0800

    update

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def get_google_image(text):
         if imgs.has_attr('id'):
             imgID = imgs['id']
             script = bfsoup.find_all('script')[7].text
-            jsonStr = script.replace('(function(){var data=[\'', '').split(';', 0)
+            jsonStr = script.replace('(function(){var data=', '').split(';for', 0)
             logging.info(jsonStr)
             imgJson = json.loads(jsonStr)
             img = imgJson[imgID]
